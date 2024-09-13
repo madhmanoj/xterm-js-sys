a26ba5995849bc3d6ee4a963198d16da05816bbe
Michael Allwright <allsey87@gmail.com>
2024-09-13 17:56:04 +0200

    Add minimal bindings for Terminal

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
-/* todo */
+use wasm_bindgen::prelude::*;
+use web_sys::HtmlDivElement;
+
+#[wasm_bindgen(module = "@xterm/xterm")]
+extern "C" {
+    #[derive(Clone)]
+    pub type Terminal;
+
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Terminal;
+
+    #[wasm_bindgen(method)]
+    pub fn open(this: &Terminal, parent: &HtmlDivElement);
+
+    #[wasm_bindgen(method)]
+    pub fn write(this: &Terminal, data: &str);
+}
+
